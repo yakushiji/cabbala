29bbe0ed5855d60d5eefd726e8673643a4131b0c
yakushiji <yaoshisizhenghe@gmail.com>
2013-07-23 18:18:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cabbala::Application.config.secret_key_base = '5bbe035cd264f836558ea85c79a5c6a62daa4f7c79d409334179d0c0d00a72e15eb7f04324333e0b1910c85a1c54d9d217a310773af54dd1832536d793261db8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cabbala::Application.config.session_store :cookie_store, key: '_cabbala_session'
